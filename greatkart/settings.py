0b4cecb4b3c9f471a4fa2ab662bb712f5ab526cd
jkseo <3designkorea@gmail.com>
2023-06-12 13:22:59 +0900

    django authentication

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -134,3 +134,16 @@ STATICFILES_DIRS = [
 # media files configuration
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
+
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',   
+}
+
+# SMTP Configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = '3designkorea@gmail.com'
+EMAIL_HOST_PASSWORD = 'sxgyrxljovkubfbh'
+EMAIL_USE_TLS = True
