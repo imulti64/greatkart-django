d3b3cda4831f1c968ba9cdf79e1d1e2b5d4cb540
jkseo <3designkorea@gmail.com>
2023-06-16 08:49:39 +0900

    product order and payments

--- store/urls.py
+++ store/urls.py
@@ -1,12 +1,6 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [    
-    path('', views.store, name='store'),
-] 
-from django.urls import path
-from . import views
-
 urlpatterns = [
     path('', views.store, name='store'),
     path('category/<slug:category_slug>/', views.store, name='products_by_category'),
